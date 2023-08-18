d859508ecf4008279877dfd6b4403893cb8c8dc9
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2023-08-19 01:54:27 +0900

    Settings

--- wallpaper_api/settings.py
+++ wallpaper_api/settings.py
@@ -1,16 +1,74 @@
 import environ
+import os
 
 BASE_DIR = environ.Path(__file__) - 1
+ROOT_DIR = environ.Path(__file__) - 2
 
-env = environ.Env()
-env_file = str(BASE_DIR.path(".env"))
-env.read_env(env_file)
+api_env = environ.Env()
+api_env_file = str(BASE_DIR.path(".env"))
+api_env.read_env(api_env_file)
 
-DEBUG = env("DEBUG", bool)
-SECRET_KEY = env("SECRET_KEY")
+db_env = environ.Env()
+db_env_file  = str(ROOT_DIR.path("./db/.env"))
+db_env.read_env(db_env_file)
+
+DEBUG = api_env("DEBUG", bool)
+
+SECRET_KEY = api_env("SECRET_KEY")
+ROOT_URLCONF = "urls"
+
+POSTGRES_USER = db_env("POSTGRES_USER")
+POSTGRES_PASSWORD = db_env("POSTGRES_PASSWORD")
 
 MEDIA_URL = "/media/"
 if DEBUG:
-    MEDIA_ROOT = BASE_DIR / "media"
+    MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
 else:
     MEDIA_ROOT = f"/var/www/{BASE_DIR.name}/media"
+    
+INSTALLED_APPS = [
+    "wallpaper",
+    "django.contrib.admin",
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.messages",
+    #"django.urls",
+    #"django.conf.settings",
+    #"django.db",
+    #"rest_framework",
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+MIDDLEWARE = [
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "wallpaper",
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
+    }
+}
---
+++ wallpaper_api/wallpaper/apps.py
@@ -0,0 +1,4 @@
+from django.apps import AppConfig
+
+class WallPaperConfig(AppConfig):
+    name = "wallpaper"
