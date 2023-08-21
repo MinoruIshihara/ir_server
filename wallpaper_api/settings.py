4241e01d56e62ad4b2252580bd7b90c65b1011cb
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2023-08-21 11:10:31 +0900

    MEDIA settings to env file

--- wallpaper_api/settings.py
+++ wallpaper_api/settings.py
@@ -4,30 +4,28 @@ import os
 BASE_DIR = environ.Path(__file__) - 1
 ROOT_DIR = environ.Path(__file__) - 2
 
+
 api_env = environ.Env()
 api_env_file = str(BASE_DIR.path(".env"))
 api_env.read_env(api_env_file)
-
-db_env = environ.Env()
-db_env_file  = str(ROOT_DIR.path("./db/.env"))
-db_env.read_env(db_env_file)
 
 DEBUG = api_env("DEBUG", bool)
 
 SECRET_KEY = api_env("SECRET_KEY")
 ROOT_URLCONF = "urls"
 
+MEDIA_URL = api_env("MEDIA_URL")
+MEDIA_ROOT = api_env("MEDIA_ROOT")
+
+
+db_env = environ.Env()
+db_env_file  = str(ROOT_DIR.path("./db/.env"))
+db_env.read_env(db_env_file)
+
 POSTGRES_USER = db_env("POSTGRES_USER")
 POSTGRES_PASSWORD = db_env("POSTGRES_PASSWORD")
 
-MEDIA_URL = "/images/"
-if DEBUG:
-    print("BASE_DIR: ", f"{BASE_DIR}/images")
-    MEDIA_ROOT = f"{BASE_DIR}/images"
-else:
-    MEDIA_ROOT = f"/var/www/{BASE_DIR.name}/media"
 
-    
 INSTALLED_APPS = [
     "wallpaper",
     "django.contrib.admin",
