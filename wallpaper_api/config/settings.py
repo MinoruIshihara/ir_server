b1e28be3b5e3d6c16df69af636d317c23d67be58
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2023-08-21 15:44:41 +0900

    server settings

--- wallpaper_api/config/settings.py
+++ wallpaper_api/config/settings.py
@@ -16,13 +16,8 @@ ROOT_URLCONF = "config.urls"
 MEDIA_URL = api_env("MEDIA_URL")
 MEDIA_ROOT = api_env("MEDIA_ROOT")
 
-
-db_env = environ.Env()
-db_env_file  = str(ROOT_DIR.path("./db/.env"))
-db_env.read_env(db_env_file)
-
-POSTGRES_USER = db_env("POSTGRES_USER")
-POSTGRES_PASSWORD = db_env("POSTGRES_PASSWORD")
+POSTGRES_USER = api_env("POSTGRES_USER")
+POSTGRES_PASSWORD = api_env("POSTGRES_PASSWORD")
 
 
 INSTALLED_APPS = [
@@ -67,7 +62,7 @@ DATABASES = {
         "NAME": "wallpaper",
         "USER": POSTGRES_USER,
         "PASSWORD": POSTGRES_PASSWORD,
-        "HOST": "127.0.0.1",
+        "HOST": "postgres-host",
         "PORT": "5432",
     }
 }
