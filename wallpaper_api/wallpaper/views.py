c8a450b47644ca36aac4f32a976361b8b0ef5231
MinoruIshihara <r116303@g.kobe-kosen.ac.jp>
2023-09-26 17:31:12 +0900

    retrieve latest image

--- wallpaper_api/wallpaper/views.py
+++ wallpaper_api/wallpaper/views.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 from django.http import HttpResponse, FileResponse
 
@@ -6,6 +7,8 @@ from rest_framework import permissions
 
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+from rest_framework.status import HTTP_404_NOT_FOUND
+
 from rest_framework.viewsets import ModelViewSet, GenericViewSet
 from rest_framework.mixins import ListModelMixin
 from rest_framework.generics import get_object_or_404
@@ -32,6 +35,16 @@ class ImageViewset(ModelViewSet):
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=HTTP_201_CREATED, headers=headers)
+
+    def retrieve(self, request, pk=None):
+        if pk == "latest":
+            objs = self.get_queryset().all()
+            latest_id = objs.latest("created_at").id
+            latest_obj = get_object_or_404(objs, pk=latest_id)
+            serializer = self.get_serializer(latest_obj)
+            return Response(data=serializer.data)
+        else:
+            return super().retrieve(self, request, pk)
 
 
 class ImageDownloadVIew(GenericViewSet, ListModelMixin):
